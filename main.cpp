8f1d3673001df0aef3afd789739cc93c40a90d1c
NikitaOlenichev <117353855+NikitaOlenichev@users.noreply.github.com>
2026-03-17 23:37:45 +0300

    Add main.cpp

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+// Подсчет хэшей
+class HashCalculate {
+
+};
+
+// Шифрование
+class Encryption {
+
+};
+
+// Дешифрование
+class Decryption {
+
+};
+
+// Генерация подписи
+class SignGenerate {
+
+};
+
+int main() {
+
+}
