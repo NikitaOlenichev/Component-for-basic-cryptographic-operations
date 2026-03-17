e5716e82e17f775b3c8cd8b4b621cfb7afc37202
Stalin33101 <savapshena13@gmail.com>
2026-03-17 23:51:00 +0300

    Add files via upload

---
+++ shifratbasha/shifratbasha.h
@@ -0,0 +1,12 @@
+#ifndef SHIFRATBASHA_H
+#define SHIFRATBASHA_H
+
+#include <string>
+
+class shifratbasha {
+public:
+    std::string encrypt(const std::string& text);
+    std::string decrypt(const std::string& text);
+};
+
+#endif
