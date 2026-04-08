30efb5d814726b08a3484ffa0882e026ad574a29
NikitaOlenichev <117353855+NikitaOlenichev@users.noreply.github.com>
2026-04-08 14:11:44 +0300

    Add main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Никита on 07.04.2026.
+//
+
+#include <iostream>
+
+#include "BigInteger.h"
+#include "EllipticPoint.h"
+#include "Streebog.h"
+#include "GOSTSignature.h"
+#include "CAdESBuilder.h"
+
+int main() {
+    // реализовать основную функцию
+}
