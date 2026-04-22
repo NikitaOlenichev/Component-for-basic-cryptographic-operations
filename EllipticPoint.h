67e0816d2bfcb20e32103a82e8e07eafd9e09960
NikitaOlenichev <117353855+NikitaOlenichev@users.noreply.github.com>
2026-04-22 16:00:57 +0300

    Update EllipticPoint.h

--- EllipticPoint.h
+++ EllipticPoint.h
@@ -1,43 +1,49 @@
 //
 // Реализованы операции с точками на эллиптической кривой: сложение и скалярное умножение.
+// Copyright (c) TheRunuwayDinosaur. All rights reserved.
+// (https://github.com/TheRunuwayDinosaur/GOST-R-34.10-12-Elliptic-Curve-Cryptography-/blob/main/sources/h/EllipticPoint.h).
 //
 
-#ifndef ELLIPTICPOINT_H
-#define ELLIPTICPOINT_H
+#ifndef ELLIPTICPOINTH_H
+#define ELLIPTICPOINTH_H
 
 #include "BigInteger.h"
 
-class EllipticPoint {
+class EllipticPoint{
 public:
-    EllipticPoint();
-    EllipticPoint(const BigInteger& x, const BigInteger& y, bool infinity = false);
+    EllipticPoint(BigInteger x_ = -1, BigInteger y_ = -1);
+    void set_A(const BigInteger &A_);
+    void set_B(const BigInteger &B_);
+    void set_p(const BigInteger &p_);
+    void set_q(const BigInteger &q_);
+    void set_mod(const BigInteger &mod_);
 
-    EllipticPoint operator+(const EllipticPoint& other) const;
-    EllipticPoint operator*(const BigInteger& scalar) const;
-    EllipticPoint& operator+=(const EllipticPoint& other);
+    BigInteger get_x() const;
+    BigInteger get_y() const;
+    BigInteger get_A() const;
+    BigInteger get_B() const;
+    BigInteger get_p() const;
+    BigInteger get_q() const;
+    BigInteger get_mod() const;
 
-    bool operator==(const EllipticPoint& other) const;
-    bool operator!=(const EllipticPoint& other) const;
+    void write_point() const;
+    bool is_inf() const;
 
-    bool isInfinity() const {
-        return infinity;
-    }
-    const BigInteger& getX() const {
-        return x;
-    }
-    const BigInteger& getY() const {
-        return y;
-    }
+    friend const EllipticPoint operator +(const EllipticPoint &left,const EllipticPoint &right);
+    friend const EllipticPoint operator *(EllipticPoint point, BigInteger x);
+    friend const BigInteger operator &(const EllipticPoint &left,const EllipticPoint &right);
 
-    static void setCurveParams(const BigInteger& p, const BigInteger& a, const BigInteger& b, const
-        BigInteger& q, const EllipticPoint& generator);
+    EllipticPoint operator +=(const EllipticPoint &right);
+    EllipticPoint operator *=(const BigInteger &x);
 
 private:
-    BigInteger x, y;
-    bool infinity;
-
-    static BigInteger p, a, b, q;
-    static EllipticPoint generator;
+    BigInteger x;
+    BigInteger y;
+    BigInteger p;
+    BigInteger q;
+    BigInteger A;
+    BigInteger B;
+    BigInteger mod;
 };
 
-#endif //ELLIPTICPOINT_H
+#endif // ELLIPTICPOINTH_H
