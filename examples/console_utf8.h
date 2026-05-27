07dbbff08e6ef0f0c87be9c6c5a71d206084dd87
Никита Оленичев <2020nikol@mail.ru>
2026-05-27 14:35:10 +0300

    Fix console_utf8.h

--- examples/console_utf8.h
+++ examples/console_utf8.h
@@ -1,16 +1,32 @@
 #pragma once
 
 #include <clocale>
+#include <locale>
+#include <iostream>
 
 #ifdef _WIN32
 #include <windows.h>
 #endif
 
 inline void setupConsoleUtf8() {
-    std::setlocale(LC_ALL, "");
-
 #ifdef _WIN32
-    SetConsoleOutputCP(CP_UTF8);
-    SetConsoleCP(CP_UTF8);
+    const char* term = getenv("TERM_PROGRAM");
+    const char* msyscon = getenv("MSYSCON");
+    bool isMintty = (term && strstr(term, "mintty")) || (msyscon && strstr(msyscon, "mintty"));
+
+    if (!isMintty) {
+        SetConsoleOutputCP(CP_UTF8);
+        SetConsoleCP(CP_UTF8);
+    }
 #endif
+
+    try {
+        std::locale::global(std::locale("en_US.UTF-8"));
+    } catch (...) {
+    }
+    std::cout.imbue(std::locale());
+    std::cerr.imbue(std::locale());
+    std::clog.imbue(std::locale());
+
+    std::setlocale(LC_ALL, ".UTF-8");
 }
